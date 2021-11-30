5c3e82040b001c5c3a5394cb97699500d0875073
TurtleArmy <44322335+TurtleArmyMc@users.noreply.github.com>
2021-11-30 11:39:05 -0500

    Updated to 1.18

--- src/main/java/com/turtlearmymc/blastproofnetherite/mixin/EntityMixin.java
+++ src/main/java/com/turtlearmymc/blastproofnetherite/mixin/EntityMixin.java
@@ -1,6 +1,8 @@
 package com.turtlearmymc.blastproofnetherite.mixin;
 
-import java.util.stream.Stream;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.util.collection.ReusableStream;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 
@@ -22,26 +23,25 @@ public abstract class EntityMixin {
     @Inject(method = "isInvulnerableTo(Lnet/minecraft/entity/damage/DamageSource;)Z", at = @At("RETURN"), cancellable = true)
     protected void isInvulnerableTo(DamageSource source, CallbackInfoReturnable<Boolean> ci) {
         Entity entity = (Entity) (Object) this;
-        if (entity instanceof ItemEntity) {
-            ItemEntity item = (ItemEntity) entity;
-            ci.setReturnValue(ci.getReturnValue()
+        if (entity instanceof ItemEntity item) {
+            ci.setReturnValue(ci.getReturnValue().booleanValue()
                     || (!item.getStack().isEmpty() && item.isFireImmune() && !source.isOutOfWorld()));
         }
     }
 
     // Covers the void in a collidable VoxelShape for fireproof items
     @ModifyVariable(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At("STORE"))
-    ReusableStream<VoxelShape> adjustMovementForCollisions(ReusableStream<VoxelShape> rs) {
-        Entity entity = (Entity) (Object) this;
-        if (entity instanceof ItemEntity) {
-            ItemEntity item = (ItemEntity) entity;
+    List<VoxelShape> adjustMovementForCollisions(List<VoxelShape> list) {
+        if ((Object) this instanceof ItemEntity item) {
             if (!item.getStack().isEmpty() && item.isFireImmune()) {
-                return new ReusableStream<VoxelShape>(Stream.concat(rs.stream(),
-                        Stream.of(VoxelShapes.cuboid(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY,
-                                Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, item.world.getBottomY(),
-                                Double.POSITIVE_INFINITY))));
+                ImmutableList.Builder<VoxelShape> builder = ImmutableList.builderWithExpectedSize(list.size() + 1);
+                builder.addAll(list);
+                builder.add(VoxelShapes.cuboid(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY,
+                        Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, item.world.getBottomY(),
+                        Double.POSITIVE_INFINITY));
+                return builder.build();
             }
         }
-        return rs;
+        return list;
     }
 }
